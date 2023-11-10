05dc86da8d4d1b46abeb5b5cc884225d597ff1cf
Satvik <satviksagar340@gmail.com>
2023-11-10 13:47:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        Hey! This is your Main Brain 💹
+       </div>
+    </>
+  )
+}
+
+export default App
